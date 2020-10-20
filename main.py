ffc4e56f51d24575889b1d0767e4ccad4e871ab1
1462326016 <1462326016@qq.com>
2020-10-20 14:15:32 +0800

    提交代码
    Signed-off-by: 1462326016 <1462326016@qq.com>

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from scrapy.cmdline import execute
+import base64
+import json
+
+import requests
 
 if __name__ == '__main__':
-    execute(['scrapy', 'crawl', 'spa1'])
+    # execute(['scrapy', 'crawl', 'spa1'])
+
+    data = {
+        'username': 'admin',
+        'password': 'admin'
+    }
+    url = 'https://login1.scrape.center/'
+    # 注意这里是 get 请求，如果是 post 请求的话会报405错误代码。
+    r = requests.get(url=url, data={'token': base64.b64encode(json.dumps(data).encode())})
+    print(r.text)
+    print(r.status_code)
