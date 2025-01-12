a83a52d2a35e76acb900c01f3f04ffe9b3399ade
libra146 <1462326016@qq.com>
2025-01-12 18:08:29 +0800

    网站改版修复
    Signed-off-by: libra146 <1462326016@qq.com>

--- learnscrapy/spiders/spa2.py
+++ learnscrapy/spiders/spa2.py
@@ -1,5 +1,4 @@
 import base64
-import random
 import time
 from hashlib import sha1
 
@@ -13,18 +12,17 @@ class SPA2Spider(scrapy.Spider):
     name = "spa2"
 
     @staticmethod
-    def sign(url):
-        t = str(int(time.time()) - random.randint(1, 20))
-        s = ','.join([url, t])
+    def sign(url, a):
+        t = str(int(time.time()))
+        s = ','.join([url, str(a), t])
         return base64.b64encode(','.join([sha1(s.encode()).hexdigest(), t]).encode()).decode()
 
     def start_requests(self):
-        urls = [
-            f'http://spa2.scrape.center/api/movie/?limit=10&offset={a}&token={self.sign("/api/movie")}' for a in
-            range(0, 100, 10)
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        for a in range(0, 100, 10):
+            token = self.sign("/api/movie", (((a + 10) // 10) - 1) * 10)
+            yield scrapy.Request(
+                url=f'http://spa2.scrape.center/api/movie/?limit=10&offset={a}&token={token}',
+                callback=self.parse)
 
     def parse(self, response, **kwargs):
         result = response.json()
@@ -37,7 +35,7 @@ class SPA2Spider(scrapy.Spider):
             item['date'] = a['published_at']
             s = 'ef34#teuq0btua#(-57w1q5o5--j@98xygimlyfxs*-!i-0-mb'
             detail = base64.b64encode((s + str(a["id"])).encode()).decode()
-            yield Request(url=response.urljoin(f'/api/movie/{detail}/?token={self.sign("/api/movie/" + detail)}'),
+            yield Request(url=response.urljoin(f'/api/movie/{detail}/?token={self.sign(f"/api/movie/{detail}", 0)}'),
                           callback=self.parse_person,
                           meta={'item': item})
 
