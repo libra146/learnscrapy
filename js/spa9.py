53da82996bde03397975a9fdd62679b9d5d2b2bd
libra146 <1462326016@qq.com>
2025-01-13 19:59:37 +0800

    添加 spa9
    Signed-off-by: libra146 <1462326016@qq.com>

--- js/spa9.py
+++ js/spa9.py
@@ -4,17 +4,19 @@ import re
 import requests
 
 
+# 转成 36 进制
 def to_base_36(num):
     digits = "0123456789abcdefghijklmnopqrstuvwxyz"
     if num == 0:
         return "0"
-    result = ""
+    r = ""
     while num > 0:
         num, remainder = divmod(num, 36)
-        result = digits[remainder] + result
-    return result
+        r = digits[remainder] + r
+    return r
 
 
+# 原 js 的解码函数，稍微去掉了一些无用的逻辑
 def ee(p, a, c, k, e, r):
     def e1(cc):
         result = ""
@@ -32,25 +34,19 @@ def ee(p, a, c, k, e, r):
         c -= 1
         r[e1(c)] = k[c]
 
-    def temp_function(e: re.Match):
-        return r.get(e.group(0), e.group(0))
-
-    c = 1
-    while c:
-        c -= 1
-        p = re.sub(r'\b[0-9a-zA-D]\b', temp_function, p)
-    return p
+    return re.sub(r'\b[0-9a-zA-D]\b', lambda e: r.get(e.group(0), e.group(0)), p)
 
 
 res = requests.get('https://spa9.scrape.center/')
-res.encoding = 'utf-8'
+res.encoding = 'utf-8'  # 不加可能会乱码
 
-content, c, k = re.findall(r"\}\('(.*?)',\[],(\d\d),'(.*?),0,\{}\)", res.text)[0]
-decode = ee(content, [], int(c), k[:-11].split('|'), 0, {})
+js_content, c, k = re.findall(r"\}\('(.*?)',\[],(\d\d),'(.*?),0,\{}\)", res.text)[0]
+# 提取出来的 js 解码函数的参数，调用自己实现的解码函数
+decode_content = ee(js_content, [], int(c), k[:-11].split('|'), 0, {})
 
-print(decode.replace('\\', ''))
+print(decode_content.replace('\\', ''))
 
-result = re.findall('players=(\[[\s\S]+\])', decode.replace('\\', ''))[0]  # type:str
+result = re.findall('players=(\[[\s\S]+\])', decode_content.replace('\\', ''))[0]  # type:str
 json_content = json.loads(result.replace('\'', '"')
                           .replace('name', '"name"')
                           .replace('image', '"image"')
