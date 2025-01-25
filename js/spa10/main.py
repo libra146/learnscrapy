2a3710c616e46b90d15c2d334a6c57b2e34cb2b5
libra146 <1462326016@qq.com>
2025-01-25 16:40:44 +0800

    add spa10
    Signed-off-by: libra146 <1462326016@qq.com>

---
+++ js/spa10/main.py
@@ -0,0 +1,33 @@
+import subprocess
+
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+
+def get_js():
+    # 可能是ssl 证书失效了，忽略异常
+    r = requests.get('https://spa10.scrape.center/js/main.js', verify=False).text
+    return r
+
+
+def run_js(code):
+    js_code = f"""
+    String.prototype.print = function() {{
+    console.log(this);
+    return this;
+    }};
+    {code.strip()[:-3]}.toString().print();
+    """
+    print(js_code)
+    process = subprocess.Popen(['node', '-e', js_code], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stderr.decode('utf-8'))
+    print(stdout.decode('utf-8'))
+    return stdout.decode('utf-8')
+
+
+if __name__ == '__main__':
+    js = get_js()
+    run_js(js)
