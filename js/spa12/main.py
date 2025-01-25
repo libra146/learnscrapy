afe7f2cbead44c352b0a80f2b1fcb956a13ba0b7
libra146 <1462326016@qq.com>
2025-01-25 23:33:56 +0800

    add spa12
    Signed-off-by: libra146 <1462326016@qq.com>

---
+++ js/spa12/main.py
@@ -0,0 +1,31 @@
+import subprocess
+
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+
+def get_js():
+    # 可能是ssl 证书失效了，忽略异常
+    r = requests.get('https://spa12.scrape.center/js/main.js', verify=False).text
+    return r
+
+
+def run_js(code):
+    js_code = f"""
+    String.prototype.print = function() {{
+    console.log(this);
+    return this;
+    }};
+    {code.strip()[:-1]}.toString().print());
+    """
+    process = subprocess.Popen(['node', '-e', js_code], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stdout.decode('utf-8'))
+    return stdout.decode('utf-8')
+
+
+if __name__ == '__main__':
+    js = get_js()
+    run_js(js)
